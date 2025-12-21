82474f117e22735b78c91e811b32ed39a6a6b500
devkaiqueteixeira <kaiqueteixeiradev@gmail.com>
2025-12-21 20:21:53 -0300

    Começando a criar a página home, criei a requisição da API e ja estou manipulando as imagens e tudo oq vem junto para exibição

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,49 @@
+import { Container } from "@/components/container";
+import { GameProps } from '@/utils/types/games'
 
-export default function Home() {
+import Link from 'next/link'
+
+import Image from 'next/image'
+async function getDalyGame() {
+  try {
+
+    const res = await fetch(`${process.env.NEXT_API_URL}/next-api/?api=game_day`)
+
+    return res.json();
+  } catch (err) {
+    throw new Error("Fala na requisição")
+  }
+}
+
+export default async function Home() {
+
+  const dalyGame: GameProps = await getDalyGame();
+
+  console.log(dalyGame);
   return (
 
-    <main className="flex">
-      <h1>Teste Page</h1>
+    <main className="w-full">
+      <div className="max-w-7xl mx-auto px-3">
+        <Container>
+
+          <h1 className="text-center front-bold text-xl mt-8 mb-5">Separamos um jogo exclusivo para você !</h1>
+          <Link href={`/game/${dalyGame.id}`}>
+            <section className="w-full bg-black rounded-lg">
+              <Image
+                src={dalyGame.image_url}
+                alt={dalyGame.title}
+                priority={true}
+                quality={100}
+                width={100}
+                height={100}
+              />
+
+
+            </section>
+          </Link>
+
+        </Container>
+      </div>
     </main>
 
   );
