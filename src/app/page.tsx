694ae88dbce18c58020b9458bd0ea56a3797588d
devkaiqueteixeira <kaiqueteixeiradev@gmail.com>
2025-12-22 21:44:48 -0300

    Melhoras da Home, a imagem do jogo do dia foi ajustada e a requisição da API foi inserido um revalidade para fazer o controle de requisição no servidor e usar o cache dentro de um espaço de tempo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,12 @@ import { GameProps } from '@/utils/types/games'
 import Link from 'next/link'
 
 import Image from 'next/image'
+
+import { BsArrowRightSquare } from 'react-icons/bs'
 async function getDalyGame() {
   try {
 
-    const res = await fetch(`${process.env.NEXT_API_URL}/next-api/?api=game_day`)
+    const res = await fetch(`${process.env.NEXT_API_URL}/next-api/?api=game_day`, { next: { revalidate: 320 } })
 
     return res.json();
   } catch (err) {
@@ -29,14 +31,21 @@ export default async function Home() {
           <h1 className="text-center front-bold text-xl mt-8 mb-5">Separamos um jogo exclusivo para você !</h1>
           <Link href={`/game/${dalyGame.id}`}>
             <section className="w-full bg-black rounded-lg">
-              <Image
-                src={dalyGame.image_url}
-                alt={dalyGame.title}
-                priority={true}
-                quality={100}
-                width={100}
-                height={100}
-              />
+              <div className="w-full max-h-96 h-96 relative rounded-lg">
+                <div className="absolute z-20 bottom-0 p-3 flex justify-center items-center gap-2">
+                  <p className="font-bold text-xl text-white">{dalyGame.title}</p>
+                  <BsArrowRightSquare size={24} color="#FFF" />
+                </div>
+                <Image
+                  src={dalyGame.image_url}
+                  alt={dalyGame.title}
+                  priority={true}
+                  quality={100}
+                  fill={true}//preenchimento total
+                  className="max-h-96 object-cover rounded-lg opacity-50 hover:opacity-100 transition-all duration-300"
+                  sizes="(max-width: 768px) 100vw, (max-width:1200px) 44vw"
+                />
+              </div>
 
 
             </section>
